15a1891335aa44bd95ac8600dd9149aafec3d02d
sunil7589 <42062109+sunil7589@users.noreply.github.com>
2020-05-25 09:19:41 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ class Test
 	public static void main(String[] args) 
 	{
 		int println=10,out=20; //local variables
-		System.out.println(println+out);
+		System.out.println(println+out);    
 	}
 }
